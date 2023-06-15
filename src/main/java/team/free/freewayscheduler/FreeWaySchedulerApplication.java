874dc67b456313b991312d91d7a800c22abcbec5
jcw1031 <jcw001031@gmail.com>
2023-06-15 18:10:53 +0900

    initial commit

---
+++ src/main/java/team/free/freewayscheduler/FreeWaySchedulerApplication.java
@@ -0,0 +1,15 @@
+package team.free.freewayscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class FreeWaySchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeWaySchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team/free/freewayscheduler/FreeWaySchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package team.free.freewayscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeWaySchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
